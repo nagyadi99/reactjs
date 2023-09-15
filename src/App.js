a8d2a06520c102dd43ca9096273d08a0b9f83bb5
nagyadi99 <nagy.adi999@gmail.com>
2023-09-15 17:11:27 +0200

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Welcome to my first React page.</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.linkedin.com/in/%C3%A1d%C3%A1m-nagy-7839001b8/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Linkedin Profile
         </a>
       </header>
     </div>
